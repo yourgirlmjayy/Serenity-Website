bf0de0cd8bd5f3a618649231e7e1e8e946edd924
Mojolajesu Mercy Dada <mojolajesu@meta.com>
2024-07-22 12:08:59 -0700

    feat: loading UI indicator for prompt fetching process

---
+++ Serenity-Website/src/Loading-Spinner/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingSpinner.css";
+
+const LoadingScreen = () => {
+  return (
+    <div className="loading-screen">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
